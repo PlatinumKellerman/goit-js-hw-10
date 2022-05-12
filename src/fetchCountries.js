2bdeaf0cd6dd6cb3b108413b93426746d09c6c80
PlatinumKellerman <platinumm@ukr.net>
2022-05-12 03:13:19 +0300

    in-progress

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,9 @@
-export  function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags.svg,languages,flags`)
-        .then((response) => response.json());
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags.svg,languages,flags`)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            }
+        })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from '../src/fetchCountries.js'
 
 
 const DEBOUNCE_DELAY = 300;
+
 
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
@@ -12,19 +14,87 @@ inputEl.addEventListener('input', onInput)
 
 function onInput() {
     const name = inputEl.value;
-    fetchCountries(name).then(country => createMarkup(country))
+    fetchCountries(name).then((country) => {
+        if (country.length <= 10 & country.length > 1) {
+
+        createCountryListMarkup(country)
+        const newCountryListMarkup = createCountryListMarkup(country);
+            countryList.innerHTML = newCountryListMarkup;
+            countyInfoThumb.innerHTML = "";
+            
+        } else if (country.length > 10) {
+            Notify.info("Too many matches found. Please enter a more specific name.", {
+                width: '400px',
+                position: 'center-top',
+                timeout: 1500,
+                borderRadius: '20px',
+                fontSize: '20px',
+                cssAnimationStyle: 'zoom',
+            })
+        } else if (country.length === 1) {
+            countryList.innerHTML = "";
+            createCountryInfoMarkup(country);
+            const countryInfoMarkup = createCountryInfoMarkup(country);
+            countyInfoThumb.innerHTML = countryInfoMarkup;
+            console.log(country);
+        }
+        
+    })
+        .catch(() => {
+        Notify.failure("Oops, there is no country with that name", {
+                width: '400px',
+                position: 'center-top',
+                timeout: 1500,
+                borderRadius: '20px',
+                fontSize: '20px',
+                cssAnimationStyle: 'zoom',
+            })
+        }).finally(e => {
+            if (name === "") {
+                countryList.innerHTML = "";
+                countyInfoThumb.innerHTML = "";
+            }
+    })
+    
 }
 
+function createCountryInfoMarkup(countries) {
+    return countries.map(country => {
+        
+        return `
+            <div class="info-wrapper">
+            <img 
+            class="flag__image"
+            width="60"
+            heigh="60"
+            src="${Object.values(country.flags)[0]}" 
+            alt="Country Flag"
+            />
+            <p class="country-info__name">${country.name}</p>
+            </div>
+        <p class="country-info">Capital: <span class="country-info__value__special">${country.capital}</span></p>
+        <p class="country-info">Population: <span class="country-info__value">${country.population}</span></p>
+        <p class="country-info">Languages: <span class="country-info__value">${Object.values(country.languages[0])[2]}</span></p>`
+    }).join('');
+}
 
+function createCountryListMarkup(countries) {
+    return countries.map(country => {
+        return `<li class="country-list__item">
+        <img 
+            class="flag__image"
+            src="${Object.values(country.flags)[0]}" 
+            alt="Country Flag"
+        />
+        <p class="country-list__name">${country.name}</p>
+    </li>`
+    }).join('');
+    }
 
-//need make markup
-function createMarkup(countries) {
-    countries.map(country => {
-        console.log(country);
-        console.log(Object.values(country.flags)[1]);           //flag link
-        console.log(country.name.official);                    //повна назва країни
-        console.log(country.capital[0]);                       //столиця
-        console.log(country.population);                      //населення
-        console.log(Object.values(country.languages));        //масив мов
-    })
-}   
+        // console.log(country);
+        // Object.values(country.flags)[1]       flag link
+        // country.name                          повна назва країни
+        // country.capital[0]                    столиця
+        // country.population       населення
+        // country.languages        масив мов
+    
